c43db924da52cc7b38a73762cf55df88c3777436
ilkp <ilkka.pokkinen@gmail.com>
2019-09-25 14:04:56 +0300

    Enemy and SoundManager classes

---
+++ Roots of Eeveel/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+	private static SoundManager Instance;
+	private List<Enemy> enemies;
+
+	void Awake()
+	{
+		if (Instance != null)
+		{
+			Destroy(gameObject);
+		}
+		Instance = this;
+		DontDestroyOnLoad(gameObject);
+	}
+
+
+	public static void makeSound(Vector3 source, float amplitude)
+	{
+
+	}
+}
