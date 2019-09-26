a45c19f8bcc3777dd86daa1e7a89e142b6eec35c
ilkp <ilkka.pokkinen@gmail.com>
2019-09-26 09:58:05 +0300

    enemy alert

--- Roots of Eeveel/Assets/Scripts/DynamicSoundObject.cs
+++ Roots of Eeveel/Assets/Scripts/DynamicSoundObject.cs
@@ -7,7 +7,6 @@ public class DynamicSoundObject : MonoBehaviour
     private void OnCollisionEnter(Collision collision)
     {
         Vector3 collisionForce = collision.impulse / Time.fixedDeltaTime;
-        Debug.Log(collisionForce.magnitude);
         SoundManager.makeSound(transform.position, collisionForce.magnitude);
     }
 }
