f330bfe3595dbedffbe3c17ff6a1273599d7680f
Woeldhin <32248067+Woeldhin@users.noreply.github.com>
2019-10-08 15:28:31 +0300

    Player dying and winning a game set to work

--- Roots of Eeveel/Assets/Scripts/GameManager.cs
+++ Roots of Eeveel/Assets/Scripts/GameManager.cs
@@ -1,14 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; private set; }
-    public Canvas gameOverScreen;
+    public Canvas endingScreen;
+    public Button resetButton;
 
     private void Awake()
     {
+        Time.timeScale = 1;
         if (Instance != null)
         {
             Destroy(this);
@@ -19,6 +23,28 @@ public class GameManager : MonoBehaviour
 
     public void SetGameOver()
     {
-        gameOverScreen.gameObject.SetActive(true);
+        endingScreen.gameObject.SetActive(true);
+        Time.timeScale = 0;
+        StartCoroutine(GameOverFadeIn());
+    }
+
+    private IEnumerator GameOverFadeIn()
+    {
+        CanvasGroup gameOverGroup = endingScreen.GetComponent<CanvasGroup>();
+        while (gameOverGroup.alpha < 1f)
+        {
+            gameOverGroup.alpha += 0.01f;
+            yield return 0;
+        }
+
+        yield return new WaitForSecondsRealtime(2);
+        resetButton.gameObject.SetActive(true);
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    public void ResetGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
---
+++ Roots of Eeveel/Assets/Scripts/Winning.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Winning : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Canvas endScreen;
+    public Button button;
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            GameManager.Instance.endingScreen = endScreen;
+            GameManager.Instance.resetButton = button;
+            GameManager.Instance.SetGameOver();
+        }
+    }
+}
