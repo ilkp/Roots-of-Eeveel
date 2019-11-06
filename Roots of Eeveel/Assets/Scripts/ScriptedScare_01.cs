184e2c10c9939545566e47680f8d2eea61c31e1d
ilkp <ilkka.pokkinen@gmail.com>
2019-11-06 11:48:51 +0200

    new highlight shaders, scaaary shadow

---
+++ Roots of Eeveel/Assets/Scripts/ScriptedScare_01.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class ScriptedScare_01 : MonoBehaviour
+{
+	private bool triggered = false;
+	private float elapsedTime = 0.0f;
+	private float speed = 8.0f;
+	[SerializeField] private GameObject capsule;
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "Player" && !triggered)
+		{
+			Debug.Log("collision");
+			triggered = true;
+			StartCoroutine(trigger());
+		}
+	}
+
+	private IEnumerator trigger()
+	{
+		while (elapsedTime < 1.0f)
+		{
+			capsule.transform.Translate(Vector3.forward * speed * Time.deltaTime);
+			elapsedTime += Time.deltaTime;
+			yield return null;
+		}
+		capsule.SetActive(false);
+	}
+}
