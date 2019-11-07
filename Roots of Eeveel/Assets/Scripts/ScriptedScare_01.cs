d5abe19f6708b6ff2cca55fe70c02e65bb016e76
ilkp <ilkka.pokkinen@gmail.com>
2019-11-07 10:57:46 +0200

    Cleaned main menu scene

--- Roots of Eeveel/Assets/Scripts/ScriptedScare_01.cs
+++ Roots of Eeveel/Assets/Scripts/ScriptedScare_01.cs
@@ -5,8 +5,8 @@ public class ScriptedScare_01 : MonoBehaviour
 {
 	private bool triggered = false;
 	private float elapsedTime = 0.0f;
-	private float speed = 8.0f;
-	[SerializeField] private GameObject capsule;
+	private float speed = 5.0f;
+	[SerializeField] private GameObject shadow;
 
 	private void OnTriggerEnter(Collider other)
 	{
@@ -22,10 +22,10 @@ public class ScriptedScare_01 : MonoBehaviour
 	{
 		while (elapsedTime < 1.0f)
 		{
-			capsule.transform.Translate(Vector3.forward * speed * Time.deltaTime);
+			shadow.transform.Translate(Vector3.forward * speed * Time.deltaTime);
 			elapsedTime += Time.deltaTime;
 			yield return null;
 		}
-		capsule.SetActive(false);
+		shadow.SetActive(false);
 	}
 }
