1a0a423f9f964e2c356b636865391508951a4aea
ilkp <ilkka.pokkinen@gmail.com>
2019-11-11 14:22:23 +0200

    Readable interactable ui element init

---
+++ Roots of Eeveel/Assets/Scripts/ScriptableObjects/ReadableData.cs
@@ -0,0 +1,13 @@
+﻿
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Readable", menuName = "Readables")]
+public class ReadableData : ScriptableObject
+{
+	public Sprite UISprite;
+	public string UIText;
+	public float TAnchorMinX;
+	public float TAnchorMaxX;
+	public float TAnchorMinY;
+	public float TAnchorMaxY;
+}
