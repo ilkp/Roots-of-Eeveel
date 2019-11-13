0d519602f9c91aa91cb58e3ebbaa7f270de268af
ilkp <ilkka.pokkinen@gmail.com>
2019-11-13 16:14:39 +0200

    tutorial scripts

---
+++ Roots of Eeveel/Assets/Scripts/Tutorial/TutorialManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class TutorialManager : MonoBehaviour
+{
+	[SerializeField] private TextMeshProUGUI tutorialText;
+	[SerializeField] private Image tutorialImage;
+
+	private List<ITutorial> tutorials;
+	private ITutorial activeTutorial;
+
+	private void Start()
+	{
+		tutorials = new List<ITutorial>() { new TutorialMovement() };
+	}
+
+	// Update is called once per frame
+	void Update()
+    {
+		if (activeTutorial != null)
+		{
+			if (activeTutorial.CheckCompletion())
+			{
+				tutorials.Remove(activeTutorial);
+				activeTutorial = null;
+			}
+		}
+		else
+		{
+			foreach (ITutorial tutorial in tutorials)
+			{
+				if (tutorial.CheckActivation())
+				{
+					activeTutorial = tutorial;
+					tutorialText.text = tutorial.HintText;
+					tutorialImage.sprite = tutorial.HintSprite;
+					break;
+				}
+			}
+		}
+    }
+}
