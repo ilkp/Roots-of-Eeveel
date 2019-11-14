348a29f97286c0280b76a45d76fca6ea772e0417
ilkp <ilkka.pokkinen@gmail.com>
2019-11-14 14:32:43 +0200

    Tutorial update

--- Roots of Eeveel/Assets/Scripts/Tutorial/TutorialMovement.cs
+++ Roots of Eeveel/Assets/Scripts/Tutorial/TutorialMovement.cs
@@ -1,4 +1,5 @@
 ﻿
+using UnityEditor;
 using UnityEngine;
 
 public class TutorialMovement : ITutorial
@@ -6,6 +7,11 @@ public class TutorialMovement : ITutorial
 	public string HintText => "Move with";
 	public Sprite HintSprite { get; set; }
 	public bool Completed { get; set; } = false;
+
+	public TutorialMovement(Sprite sprite)
+	{
+		HintSprite = sprite;
+	}
 
 	public bool CheckActivation()
 	{
